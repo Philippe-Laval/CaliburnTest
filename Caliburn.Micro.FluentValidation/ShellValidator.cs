9c898bc97eb0262bef7f07e7d23a53232e2ab53e
Philippe Laval <philippe.laval@free.fr>
2014-11-06 22:38:30 +0100

    Add validation

---
+++ Caliburn.Micro.FluentValidation/ShellValidator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+using FluentValidation;
+
+namespace Caliburn.Micro.FluentValidation
+{
+    class ShellValidator : AbstractValidator<ShellViewModel>
+    {
+        public ShellValidator()
+        {
+            RuleFor(shell => shell.Name)
+                .NotEmpty()
+                .WithMessage("Please Specify a Name.");
+
+            RuleFor(shell => shell.Email)
+                .EmailAddress()
+                .WithMessage("Please Specify a Valid E-Mail Address");
+
+            RuleFor(shell => shell.Zip)
+                .Must(BeAValidZip)
+                .WithMessage("Please Enter a Valid Zip Code");
+        }
+
+        private static bool BeAValidZip(string zip)
+        {
+            if (!string.IsNullOrEmpty(zip))
+            {
+                var regex = new Regex(@"\d{5}");
+                return regex.IsMatch(zip);
+            }
+            return false;
+        }
+    }
+}
--- Caliburn.Micro.FluentValidation/ShellViewModel.cs
+++ Caliburn.Micro.FluentValidation/ShellViewModel.cs
@@ -1,12 +1,24 @@
 ﻿namespace Caliburn.Micro.FluentValidation
 {
+    using System;
+    using System.ComponentModel;
     using System.ComponentModel.Composition;
     using System.Windows;
+    using System.Linq;
+    using FluentValidation;
 
     [Export(typeof(IShell))]
-    public class ShellViewModel : PropertyChangedBase, IShell
+    public class ShellViewModel : PropertyChangedBase, IShell, IDataErrorInfo
     {
+        private readonly ShellValidator _shellValidator;
         string name;
+        string email;
+        string zip;
+
+        public ShellViewModel()
+        {
+            _shellValidator = new ShellValidator();
+        }
 
         public string Name
         {
@@ -15,18 +27,75 @@
             {
                 name = value;
                 NotifyOfPropertyChange(() => Name);
+                NotifyOfPropertyChange(() => Error);
+                NotifyOfPropertyChange(() => CanSayHello);
+            }
+        }
+
+        public string Email
+        {
+            get { return email; }
+            set
+            {
+                email = value;
+                NotifyOfPropertyChange(() => Email);
+                NotifyOfPropertyChange(() => Error);
+                NotifyOfPropertyChange(() => CanSayHello);
+            }
+        }
+
+        public string Zip
+        {
+            get { return zip; }
+            set
+            {
+                zip = value;
+                NotifyOfPropertyChange(() => Zip);
+                NotifyOfPropertyChange(() => Error);
                 NotifyOfPropertyChange(() => CanSayHello);
             }
         }
 
         public bool CanSayHello
         {
-            get { return !string.IsNullOrWhiteSpace(Name); }
+ //           get { return !string.IsNullOrWhiteSpace(Name); }
+            get { return string.IsNullOrWhiteSpace(Error); }
         }
 
         public void SayHello()
         {
             MessageBox.Show(string.Format("Hello {0}!", Name));
         }
+
+        #region IDataErrorInfo
+        public string this[string columnName]
+        {
+            get
+            {
+                var firstOrDefault = _shellValidator.Validate(this).Errors.FirstOrDefault(lol => lol.PropertyName == columnName);
+                if (firstOrDefault != null)
+                    return _shellValidator != null ? firstOrDefault.ErrorMessage : "";
+                return "";
+            }
+        }
+
+        public string Error
+        {
+            get
+            {
+                if (_shellValidator != null)
+                {
+                    var results = _shellValidator.Validate(this);
+                    if (results != null && results.Errors.Any())
+                    {
+                        var errors = string.Join(Environment.NewLine, results.Errors.Select(x => x.ErrorMessage).ToArray());
+                        return errors;
+                    }
+                }
+                return string.Empty;
+            }
+        }
+        #endregion
+
     }
 }
