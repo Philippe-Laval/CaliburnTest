b2bf612dd00c4636c3e77eb2553c47244934b561
Philippe Laval <philippe.laval@free.fr>
2014-11-06 22:05:01 +0100

    First version without validation

---
+++ Caliburn.Micro.FluentValidation/MefBootstrapper.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+using System.ComponentModel.Composition.Primitives;
+using System.Linq;
+using System.Windows;
+
+namespace Caliburn.Micro.FluentValidation
+{
+    public class MefBootstrapper : BootstrapperBase
+    {
+        private CompositionContainer container;
+
+        public MefBootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void Configure()
+        {
+            AggregateCatalog catalog = new AggregateCatalog(
+                    AssemblySource.Instance.Select(x => new AssemblyCatalog(x)).OfType<ComposablePartCatalog>()
+                    );
+
+#if SILVERLIGHT
+            container = CompositionHost.Initialize(catalog);
+#else
+            container = new CompositionContainer(catalog);
+#endif
+
+            var batch = new CompositionBatch();
+
+            batch.AddExportedValue<IWindowManager>(new WindowManager());
+            batch.AddExportedValue<IEventAggregator>(new EventAggregator());
+            batch.AddExportedValue(container);
+
+            container.Compose(batch);
+        }
+
+        protected override object GetInstance(Type serviceType, string key)
+        {
+            var contract = string.IsNullOrEmpty(key) ? AttributedModelServices.GetContractName(serviceType) : key;
+            var exports = container.GetExportedValues<object>(contract);
+
+            if (exports.Any())
+                return exports.First();
+
+            throw new Exception(string.Format("Could not locate any instances of contract {0}.", contract));
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type serviceType)
+        {
+            return container.GetExportedValues<object>(AttributedModelServices.GetContractName(serviceType));
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            container.SatisfyImportsOnce(instance);
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<IShell>();
+        }
+    }
+}
